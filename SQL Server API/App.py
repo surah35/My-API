980e99dc523dfc7d59e384a5b4ad38b141157360
Surah111 <f20040706@gmail.com>
2025-09-06 00:32:24 +0800

    新增 資料庫新增資料、刪除資料功能

--- SQL Server API/App.py
+++ SQL Server API/App.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import pymssql
+from pydantic import BaseModel
 import os
 from dotenv import load_dotenv
 
@@ -14,6 +15,11 @@ PASSWORD = os.getenv("DB_PASSWORD")
 DBNAME = "Virtual_Store"
 TBNAME = "store_data"
 
+class StoreItem(BaseModel):
+    ID: int
+    Name: str
+    Price: int
+    Num:int
 
 @app.get("/")
 def read_root():
@@ -46,6 +52,47 @@ def get_data():
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
     
+# 新增資料
+@app.post("/api/data")
+def insert_data(item: StoreItem):
+    try:
+        conn = pymssql.connect(
+            server=SERVER,
+            charset='UTF-8',
+            user=USER,
+            password=PASSWORD,
+            database=DBNAME,
+        )
+        cursor = conn.cursor()
+        cursor.execute(
+            f"INSERT INTO {TBNAME} (ID, Name, Price, Num) VALUES (%s, %s, %s, %s)",
+            (item.ID, item.Name, item.Price, item.Num)
+        )
+        conn.commit()
+        conn.close()
+        return JSONResponse(content={"message": "資料新增成功", "data": item.dict()})
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+# 刪除資料
+@app.delete("/api/data/{id}")
+def delete_data(id: int):
+    try:
+        conn = pymssql.connect(
+            server=SERVER,
+            charset='UTF-8',
+            user=USER,
+            password=PASSWORD,
+            database=DBNAME,
+        )
+        cursor = conn.cursor()
+        cursor.execute(f"DELETE FROM {TBNAME} WHERE ID = %s", (id,))
+        conn.commit()
+        conn.close()
+        return JSONResponse(content={"message": f"ID {id} 已刪除"})
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
 
 if __name__ == "__main__":
